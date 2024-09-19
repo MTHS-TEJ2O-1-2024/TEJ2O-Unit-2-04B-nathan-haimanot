40f70dae8a119379fa2cb608665c47187bb269bc
nathan-haimanot <nathan.haimanot@mths.ca>
2024-09-19 13:51:18 -0400

    made a dice and changed comments

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nathan Haimanot
+ * Created on: Sep 2024
+ * This program simulates a 6 sided dice
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    randomNumber = randint(1, 6)
+    basic.showNumber(randomNumber)
+})
+
+
+
+basic.showString('Hello, Nathan!')
