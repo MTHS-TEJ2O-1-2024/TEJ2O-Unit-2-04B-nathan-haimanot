3042007711ccbfef5ef02e4dc05a89ab845484cc
nathan-haimanot <nathan.haimanot@mths.ca>
2024-09-20 10:29:55 -0400

    changed comments

--- main.ts
+++ main.ts
@@ -5,16 +5,6 @@
  * This program simulates a 6 sided dice
 */
 
-let randomNumber: number
-
-basic.clearScreen()
-basic.showIcon(IconNames.Happy)
-
-input.onButtonPressed(Button.A, function () {
-    randomNumber = randint(1, 6)
-    basic.showNumber(randomNumber)
-})
-
 
 
 basic.showString('Hello, Nathan!')
